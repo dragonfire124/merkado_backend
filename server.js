220de2ec6bcef0d304982025f90ef46a51575267
Carlos Chavez <whitefish@hotmail.com>
2022-11-30 21:07:30 -0600

    se agrega dotenv

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ import uploadRouter from './routers/uploadRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import productRouter from './routers/productRouter.js';
 import User from './models/userModels.js';
+import dotenv from 'dotenv'
 
-
+dotenv.config();
 mongoose.connect(
     'mongodb+srv://karl:holamundo@cluster0.uxvrmgu.mongodb.net/test',{
     useNewUrlParser:true,
@@ -22,9 +23,6 @@ mongoose.connect(
 
 const app = express();
 
-
-
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/users', userRouter);
@@ -34,15 +32,14 @@ app.use('/api/products', productRouter);
 
 
 
-
-
 /*
 app.use((err,req,res)=>{
     const status = err.name && err.name === 'ValidationError'? 400:500;
     res.status(status).send({message: err.message})
   })*/
  
-  app.listen(5000, () => {
-    console.log('serve at http://localhost:5000');
+  const port = process.env.PORT || 5000
+  app.listen(port, () => {
+    console.log(`server at ${port}`);
   });
   
